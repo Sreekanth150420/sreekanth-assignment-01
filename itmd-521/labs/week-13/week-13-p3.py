04623e55712b4d04922799d25400f89aecfd63fb
sreekanth <sthupakula@hawk.iit.edu>
2024-04-23 22:27:19 -0500

    part 3

---
+++ itmd-521/labs/week-13/week-13-p3.py
@@ -0,0 +1,40 @@
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+
+# Removing hard coded password - using os module to import them
+import os
+import sys
+
+conf = SparkConf()
+conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.3.0')
+conf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider')
+
+conf.set('spark.hadoop.fs.s3a.access.key', 'sthupakula')
+conf.set('spark.hadoop.fs.s3a.secret.key', 'cb1d7110-eb0a-11ee-b25d-8f5b728a3f7f')
+# Configure these settings
+# https://medium.com/@dineshvarma.guduru/reading-and-writing-data-from-to-minio-using-spark-8371aefa96d2
+conf.set("spark.hadoop.fs.s3a.path.style.access", "true")
+conf.set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+# https://github.com/minio/training/blob/main/spark/taxi-data-writes.py
+# https://spot.io/blog/improve-apache-spark-performance-with-the-s3-magic-committer/
+conf.set('spark.hadoop.fs.s3a.committer.magic.enabled','true')
+conf.set('spark.hadoop.fs.s3a.committer.name','magic')
+# Internal IP for S3 cluster proxy
+conf.set("spark.hadoop.fs.s3a.endpoint", "http://infra-minio-proxy-vm0.service.consul")
+
+spark = SparkSession.builder.appName("sthupakula p3 ").config('spark.driver.host','spark-edge.service.consul').config(conf=conf).getOrCreate()
+
+
+splitDF = spark.read.parquet('s3a://sthupakula/80.parquet')
+averagedf = splitDF.select(month(col('ObservationDate')).alias('Month'),year(col('ObservationDate')).alias('Year'),col('AirTemperature').alias('Temperature'))\
+             .where((col('Temperature') > -50) & (col('Temperature') < 50)).groupBy('Month','Year').agg(avg('Temperature').alias('average')).orderBy('Year','Month')
+averagedf.show()
+stddf = avg_df.select(col('Month'),col('average')).groupBy('Month').agg(stddev('average'))
+stddf.show()
+
+stddf.write.mode("overwrite").parquet("s3a://sthupakula/part-three.parquet")
+
+splitDF.coalesce(1).write.mode("overwrite").option("header","true").csv("s3a://sthupakula/part-three.csv")
+
